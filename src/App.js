2723cdb11d10087d1a2efd393fa22af2cf7283ad
Huguez <carlos.huguez@gmail.com>
2020-07-13 22:39:23 -0700

    se creo el repositorio con el proyecto ya empezado

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+// import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+import Msn from './Msn/Msn';
+
+class App extends Component {
+    state = {
+        persons:[ 
+            {name:"Huguez" , edad: 30 },
+            {name:"Sylvia" , edad: 20 },
+            {name:"Erick" , edad: 25 },
+        ],
+        otherState: "otro estado de prueba"
+    }
+    
+    atributo = "esto es la variable";
+
+    cambiarNombre = (NuevoNombre) => {
+        this.setState({ persons: [ 
+            {name: NuevoNombre , edad: 30 },
+            {name: "Ilyana" , edad: 20 },
+            {name: "Fernando" , edad: 25 },
+        ] })
+    }
+
+    NombreCambiado = (event) => {
+        this.setState({
+            persons: [ 
+                { name: "El Huguez" , edad: 30 },
+                { name: event.target.value , edad: 25 },
+                { name: "Ilyana" , edad: 20 },
+            ]
+        });
+    };
+
+    render() {
+        const style ={
+            backgroundColor: 'green',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px'
+        };
+
+        return ( 
+            <div className = "App" >
+                <h1>Hi, I'm a React App</h1>
+                    <p>{ this.atributo } Est o es una practica.</p>
+
+                    <button style={style}  onClick={() => this.cambiarNombre( "Carlos" ) } >
+                        Cambiar Nombre
+                    </button>
+                    
+                    <Person 
+                    name={ this.state.persons[0].name } 
+                    edad={ this.state.persons[0].edad } > <Msn/> </Person>
+                    
+                    <Person 
+                    func={ this.cambiarNombre.bind( this, "Alonso" ) } 
+                    click={ this.cambiarNombre.bind( this, "Sylvi" ) }
+                    changed={ this.NombreCambiado }
+                    name={ this.state.persons[1].name } 
+                    edad={ this.state.persons[1].edad } > <Msn hobies="y mis hobbies son: ..." /> </Person>
+                    
+                    <Person 
+                    name={ this.state.persons[2].name } 
+                    edad={ this.state.persons[2].edad } > y mis hobbies son: ...  </Person>
+            </div>
+        );
+        // return React.createElement( 'div', null, React.createElement( 'h1', null, "Hi, I'm a React App" ) );
+    }
+}
+
+export default App;
+
+// const app = ( props ) => {
+//     const [ personsState, setPersonsState ] = useState({
+//         persons:[ 
+//             {name:"Huguez" , edad: 30 },
+//             {name:"Sylvia" , edad: 20 },
+//             {name:"Erick" , edad: 25 },
+//         ],
+//         otherState: "otro estado de prueba"
+//     });
+    
+//     const [ other, setOther ] = useState({
+//         otherState: "otro valor de prueba"
+//     });
+
+//     const cambiarNombre = () => {
+//         setPersonsState( {
+//             persons: [ 
+//                 { name: "Carlos", edad: 30 },
+//                 { name: "Ilyana", edad: 20 },
+//                 { name: "Fernando", edad: 25 },
+//             ],
+//             otherState: " otro estado de prueba"
+//         });
+//     };
+
+//     return ( 
+//         <div className = "App" >
+//             <h1>Hi, I'm a React App</h1>
+//                 <p>{ this.variable } y esto es una practica.</p>
+//                 <button onClick={ cambiarNombre } >Cambiar Nombre</button>
+//                 <Person name={ personsState.persons[0].name } edad={ personsState.persons[0].edad } > <Msn/> </Person>
+//                 <Person name={ personsState.persons[1].name } edad={ personsState.persons[1].edad } > <Msn hobies="y mis hobbies son: ..." /> </Person>
+//                 <Person name={ personsState.persons[2].name } edad={ personsState.persons[2].edad } > y mis hobbies son: ...  </Person>
+//         </div>
+//     );
+//     // return React.createElement( 'div', null, React.createElement( 'h1', null, "Hi, I'm a React App" ) );
+// }
+
+// export default app;
---
+++ src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './Person.css';
+
+
+const person = (props) => {
+    let edad   = props.edad;
+    let nombre = props.name;
+    return (
+        <div className="Person">
+            <h2 onClick={ props.func } > Soy { nombre } con { edad } years !! </h2>
+            <p > { props.children } </p>
+            <input type="text"  onChange={ props.changed } value={ props.name }  />
+        </div>
+    );
+};
+
+export default person;
