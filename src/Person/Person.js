e50c7ad3e37e5af3a4acae0ea054e662465fd2a9
Huguez <carlos.huguez@gmail.com>
2020-07-16 20:59:17 -0700

    se realizo el 'empaquetado' del css tambien en persona

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,22 +1,9 @@
 import React from "react";
+import clases from './Person.css';
 
 // import './Person.css';
 // import Radium from "radium";
 import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 10px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    padding-top: 0px;
-    text-align: center;
-
-    @media( min-width: 500px ){
-        width: 450px;
-    }
-`;
 
 const person = (props) => {
     let edad   = props.edad;
@@ -29,16 +16,16 @@ const person = (props) => {
     // };
 
     return (
-        // <div className="Person"  style={estilo}>
-         <StyledDiv>
+        <div className={clases.Person} >
+        {/* //  <StyledDiv> */}
             <h2 onClick={ props.click } > Soy { nombre } con { edad } years !! </h2>
             <p> { props.children } </p>
             <input
                 type="text"
                 onChange={ props.changed } 
                 value={ props.name }  />
-        </StyledDiv>
-        //  </div>
+        {/* // </StyledDiv> */}
+        </div>
     );
 };
 
